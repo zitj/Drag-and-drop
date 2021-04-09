c41e668023d0342fcd36eafc271bebc17116d192
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-04-09 13:00:34 +0200

    Improve UI grab and drop

--- script.js
+++ script.js
@@ -2,20 +2,42 @@ console.log('We are connected!');
 const lists = document.querySelectorAll('ul');
 const items = document.querySelectorAll('.item');
 const colons = document.querySelectorAll('.colon');
-const buttons = document.querySelectorAll('#deleteBtn');
+const buttons = document.querySelectorAll('.deleteBtn');
 
-console.log(items);
-console.log(lists);
-console.log(colons);
-console.log(buttons);
+let isLeft = true;
 
 items.forEach((el) =>
-    el.addEventListener('click', (e) => {
-        console.log(e.currentTarget.id);
+    el.addEventListener('mousedown', (event) => {
+        console.log(el.clientWidth);
+        console.log(event);
+        if (event.clientX >= 750) {
+            isLeft = false;
+        } else {
+            isLeft = true;
+        }
+        if (isLeft) {
+            event.currentTarget.style.transform = 'rotate(-2deg) scale(1.02)';
+        } else {
+            event.currentTarget.style.transform = 'rotate(2deg) scale(1.02)';
+        }
     })
 );
+items.forEach((el) => {
+    el.addEventListener('mouseup', (event) => {
+        event.currentTarget.style.transform = 'rotate(0deg) scale(1)';
+    });
+});
+
+document.addEventListener('dragend', function (event) {
+    event.target;
+    event.target.style.transform = 'rotate(0deg) scale(1)';
+});
+
 buttons.forEach((el) =>
     el.addEventListener('click', (e) => {
         console.log(e.currentTarget);
+        items.forEach((item) => {
+            item.style.transform = 'rotate(0deg) scale(1)';
+        });
     })
 );
