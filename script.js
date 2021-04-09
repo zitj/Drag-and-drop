e36532a45f189bf58747e4fbbb814ad2ef9eea5b
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-04-09 11:07:19 +0200

    Declare vars in JS file

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('We are connected!');
+const lists = document.querySelectorAll('ul');
+const items = document.querySelectorAll('.item');
+const colons = document.querySelectorAll('.colon');
+const buttons = document.querySelectorAll('#deleteBtn');
+
+console.log(items);
+console.log(lists);
+console.log(colons);
+console.log(buttons);
+
+items.forEach((el) =>
+    el.addEventListener('click', (e) => {
+        console.log(e.currentTarget.id);
+    })
+);
+buttons.forEach((el) =>
+    el.addEventListener('click', (e) => {
+        console.log(e.currentTarget);
+    })
+);
