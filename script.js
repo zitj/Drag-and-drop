4578ba1463d7d7aa12b4e822ceadaa2b361faa16
Aleksandar Veljkovic <1eyedea2eyedeas@gmail.com>
2021-04-13 16:18:07 +0200

    Restyle lists

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ const buttons = document.querySelectorAll('.deleteBtn');
 
 let isLeft = true;
 
-items.forEach((el) =>
+items.forEach((el) => {
     el.addEventListener('mousedown', (event) => {
-        console.log(el.clientWidth);
-        console.log(event);
         if (event.clientX >= 750) {
             isLeft = false;
         } else {
@@ -20,14 +18,56 @@ items.forEach((el) =>
         } else {
             event.currentTarget.style.transform = 'rotate(2deg) scale(1.02)';
         }
-    })
-);
-items.forEach((el) => {
+    });
     el.addEventListener('mouseup', (event) => {
         event.currentTarget.style.transform = 'rotate(0deg) scale(1)';
     });
+    el.addEventListener('dragstart', (event) => {
+        el.classList.add('dragging');
+    });
+    el.addEventListener('dragend', (event) => {
+        el.classList.remove('dragging');
+    });
 });
 
+lists.forEach((list) => {
+    list.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        const draggable = document.querySelector('.dragging');
+
+        list.style.background = 'rgb(244, 244, 244)';
+    });
+    list.addEventListener('dragexit ', (e) => {
+        list.style.background = 'white';
+    });
+});
+
+// document.addEventListener(
+//     'dragover',
+//     function (event) {
+//         event.preventDefault();
+//     },
+//     false
+// );
+// document.addEventListener(
+//     'dragenter',
+//     function (event) {
+//         if (event.target.className == 'colon') {
+//             event.target.style.background = 'purple';
+//         }
+//     },
+//     false
+// );
+// document.addEventListener(
+//     'dragleave',
+//     function (event) {
+//         // reset background of potential drop target when the draggable element leaves it
+//         if (event.target.className == 'colon') {
+//             event.target.style.background = '';
+//         }
+//     },
+//     false
+// );
 document.addEventListener('dragend', function (event) {
     event.target;
     event.target.style.transform = 'rotate(0deg) scale(1)';
@@ -35,7 +75,6 @@ document.addEventListener('dragend', function (event) {
 
 buttons.forEach((el) =>
     el.addEventListener('click', (e) => {
-        console.log(e.currentTarget);
         items.forEach((item) => {
             item.style.transform = 'rotate(0deg) scale(1)';
         });
